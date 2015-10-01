92c501665726c4b11ab77806e94a738d4eee0e16
JBH92 <jordan.bh@outlook.com>
2015-10-01 11:25:56 +0100

    changed pull script

--- public/gitScripts/pull.sh
+++ public/gitScripts/pull.sh
@@ -8,4 +8,8 @@ git stash || echo "No changes to stash"
 
 git pull || echo "Already up to date"
 
-git stash apply || echo "No changes were stashed"
+git stash pop || echo "No changes were stashed"
+
+git merge -s recursive -Xours || echo "No conflicts to merge"
+
+git commit -m "Auto merged" || echo "Nothing to commit"
